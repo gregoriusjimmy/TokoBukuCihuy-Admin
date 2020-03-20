7c3e728fedc17c3ea8bd9e25799cf01a132e87db
gregoriusjimmy <gregoriusjimmy@gmail.com>
2020-03-20 15:24:04 +0700

    changing url api to herku url

--- Bookstore.js
+++ Bookstore.js
@@ -1,23 +1,30 @@
 // Bookstore class to handle CRUD of the book
 class Bookstore {
   static async getBooks() {
-    let response = await fetch('http://localhost:3000/api/books');
+    let response = await fetch(
+      'https://tokobuku-cihuy.herokuapp.com/api/books'
+    );
     let data = await response.json();
     return data;
   }
 
   static async getBookById(bookId) {
-    let response = await fetch('http://localhost:3000/api/books/' + bookId);
+    let response = await fetch(
+      'https://tokobuku-cihuy.herokuapp.com/api/books' + bookId
+    );
     let data = await response.json();
     return data;
   }
 
   static async addBook(dataReceived) {
-    let response = await fetch('http://localhost:3000/api/books', {
-      method: 'post',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(dataReceived),
-    });
+    let response = await fetch(
+      'https://tokobuku-cihuy.herokuapp.com/api/books',
+      {
+        method: 'post',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(dataReceived),
+      }
+    );
     if (response.status === 200) {
       return true;
     } else {
@@ -25,11 +32,14 @@ class Bookstore {
     }
   }
   static async updateBook(dataReceived) {
-    let response = await fetch('http://localhost:3000/api/books', {
-      method: 'put',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(dataReceived),
-    });
+    let response = await fetch(
+      'https://tokobuku-cihuy.herokuapp.com/api/books',
+      {
+        method: 'put',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(dataReceived),
+      }
+    );
     if (response.status === 200) {
       return true;
     } else {
@@ -37,11 +47,14 @@ class Bookstore {
     }
   }
   static async delBook(dataReceived) {
-    let response = await fetch('http://localhost:3000/api/books', {
-      method: 'delete',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(dataReceived),
-    });
+    let response = await fetch(
+      'https://tokobuku-cihuy.herokuapp.com/api/books',
+      {
+        method: 'delete',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(dataReceived),
+      }
+    );
     if (response.status === 200) {
       return true;
     } else {
