cda11c731f6de24447c777679d447158cbe6403f
Gregorius Jimmy <gregoriusjimmy@gmail.com>
2021-05-06 13:01:17 +0700

    empty commit

--- controller/login.js
+++ controller/login.js
@@ -1,26 +1,30 @@
 const handleLoginPost = (req, res, pool) => {
-  const username = req.body.username;
-  const password = req.body.password;
+  const username = req.body.username
+  const password = req.body.password
+  console.log(username, password)
   if (username && password) {
     pool.query(
       'SELECT * FROM admin WHERE username = $1 AND password = $2',
       [username, password],
       (error, results) => {
+        console.log(results)
         if (results.rows.length > 0) {
-          req.session.loggedin = true;
-          req.session.username = username;
-          res.redirect('/admin');
+          req.session.loggedin = true
+          req.session.username = username
+          console.log('redirect to admin')
+          res.redirect('/admin')
         } else {
-          res.send('Incorrect Username and/or Password!');
+          res.send('Incorrect Username and/or Password!')
+          console.log('failed')
         }
-        res.end();
+        res.end()
       }
-    );
+    )
   } else {
-    res.send('Please enter Username and Password!');
+    res.send('Please enter Username and Password!')
   }
-};
+}
 
 module.exports = {
   handleLoginPost,
-};
+}
